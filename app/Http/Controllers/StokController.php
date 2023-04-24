d6c80b8884d23a07766c0bad4aca9701e81c62d9
ibnu56 <x6perb@gmail.com>
2023-04-24 19:54:34 +0700

    Update StokController.php

--- app/Http/Controllers/StokController.php
+++ app/Http/Controllers/StokController.php
@@ -3,52 +3,53 @@
 namespace App\Http\Controllers;
 
 use App\Models\Stok;
+use Barryvdh\DomPDF\Facade\Pdf;
+// use PDF;
 use Illuminate\Http\Request;
-use PDF;
 
 class StokController extends Controller
 {
     public function stokTampil()
     {
-        $stok = Stok::join('tb_parfum','tb_stok.kode_barang','=','tb_parfum.kode_barang')
-        ->join('tb_agen','tb_stok.kode_agen','=','tb_agen.kode_agen')
-        ->select('tb_stok.*','tb_parfum.nama_barang','tb_agen.nama_agen')
-        ->get();
-        $stok1 = Stok::join('tb_parfum','tb_stok.kode_barang','=','tb_parfum.kode_barang')
-        ->join('tb_agen','tb_stok.kode_agen','=','tb_agen.kode_agen')
-        ->select('tb_stok.*','tb_parfum.nama_barang','tb_agen.nama_agen')
-        ->get();
+        $stok = Stok::join('tb_parfum', 'tb_stok.kode_barang', '=', 'tb_parfum.kode_barang')
+            ->join('tb_agen', 'tb_stok.kode_agen', '=', 'tb_agen.kode_agen')
+            ->select('tb_stok.*', 'tb_parfum.nama_barang', 'tb_agen.nama_agen')
+            ->get();
+        $stok1 = Stok::join('tb_parfum', 'tb_stok.kode_barang', '=', 'tb_parfum.kode_barang')
+            ->join('tb_agen', 'tb_stok.kode_agen', '=', 'tb_agen.kode_agen')
+            ->select('tb_stok.*', 'tb_parfum.nama_barang', 'tb_agen.nama_agen')
+            ->get();
         // dd($stok);
-        return view('lapor/stok/stok1',[
+        return view('lapor/stok/stok1', [
             'STok' => $stok,
-            'STok1'=> $stok1,
+            'STok1' => $stok1,
         ]);
     }
 
     public function stokAgen(Request $request)
     {
         $dar = $request->nama;
-        $stok = Stok::join('tb_parfum','tb_stok.kode_barang','=','tb_parfum.kode_barang')
-        ->join('tb_agen','tb_stok.kode_agen','=','tb_agen.kode_agen')
-        ->where('tb_stok.kode_agen','=',$dar)
-        ->select('tb_stok.*','tb_parfum.nama_barang','tb_agen.nama_agen')
-        ->get();
-        $stok1 = Stok::join('tb_parfum','tb_stok.kode_barang','=','tb_parfum.kode_barang')
-        ->join('tb_agen','tb_stok.kode_agen','=','tb_agen.kode_agen')
-        ->select('tb_stok.*','tb_parfum.nama_barang','tb_agen.nama_agen')
-        ->get();
-        return view('lapor/stok/stok1',[
+        $stok = Stok::join('tb_parfum', 'tb_stok.kode_barang', '=', 'tb_parfum.kode_barang')
+            ->join('tb_agen', 'tb_stok.kode_agen', '=', 'tb_agen.kode_agen')
+            ->where('tb_stok.kode_agen', '=', $dar)
+            ->select('tb_stok.*', 'tb_parfum.nama_barang', 'tb_agen.nama_agen')
+            ->get();
+        $stok1 = Stok::join('tb_parfum', 'tb_stok.kode_barang', '=', 'tb_parfum.kode_barang')
+            ->join('tb_agen', 'tb_stok.kode_agen', '=', 'tb_agen.kode_agen')
+            ->select('tb_stok.*', 'tb_parfum.nama_barang', 'tb_agen.nama_agen')
+            ->get();
+        return view('lapor/stok/stok1', [
             'STok' => $stok,
-            'STok1'=> $stok1,
+            'STok1' => $stok1,
         ]);
     }
     public function laporCetak()
     {
-        $stok = Stok::join('tb_parfum','tb_stok.kode_barang','=','tb_parfum.kode_barang')
-        ->join('tb_agen','tb_stok.kode_agen','=','tb_agen.kode_agen')
-        ->select('tb_stok.*','tb_parfum.nama_barang','tb_agen.nama_agen')
-        ->get();
-        $pdf = PDF::loadview('lapor.stok.stok_pdf',['STok'=>$stok]);
-        return $pdf->download('Laporan-Stok-pdf');
+        $stok = Stok::join('tb_parfum', 'tb_stok.kode_barang', '=', 'tb_parfum.kode_barang')
+            ->join('tb_agen', 'tb_stok.kode_agen', '=', 'tb_agen.kode_agen')
+            ->select('tb_stok.*', 'tb_parfum.nama_barang', 'tb_agen.nama_agen')
+            ->get();
+        $pdf = Pdf::loadview('lapor.stok.stok_pdf', ['STok' => $stok]);
+        return $pdf->download('Laporan-Stok.pdf');
     }
 }
